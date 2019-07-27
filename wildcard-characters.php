70ac42766bef81e095b08ca3e08784936a7b5eb7
Jeremy Reyes B <jjrb6@hotmail.com>
2019-07-27 10:58:49 -0500

    [feat]: Solved challenge.

--- wildcard-characters.php
+++ wildcard-characters.php
@@ -1,5 +1,21 @@
 <?php
-
+/**
+ * Challenge
+ *
+ * Have the function WildcardCharacters(str) read str which will contain two strings separated by a space.
+ * The first string will consist of the following sets of characters: +, *, and {N} which is optional. The plus (+)
+ * character represents a single alphabetic character, the asterisk (*) represents a sequence of the same character of
+ * length 3 unless it is followed by {N} which represents how many characters should appear in the sequence where N
+ * will be at least 1. Your goal is to determine if the second string exactly matches the pattern of the first string
+ * in the input.
+ *
+ * For example: if str is "++*{5} gheeeee" then the second string in this case does match the pattern, so your
+ * program should return the string true. If the second string does not match the pattern your program should
+ * return the string false.
+ *
+ * Hard challenges are worth 15 points and you are not timed for them.
+ *
+ */
 
 /**
  * @param string $str
@@ -42,13 +58,33 @@ function WildcardCharacters(string $str): bool
                     $key = $quantity['position'];
                     $countString += $quantity['value'];
                 }
+                else {
+
+                    if (!verifySpecialCharacter($results, $countString, 3)) {
+                        return false;
+                    }
+
+                    $countString += 3;
+                }
+
+                break;
+
+            case '{':
+
+                $quantity = getQuantityBySpecialCharacter($charList, $key);
+
+                if (!$quantity || !verifySpecialCharacter($results, $countString, (int)$quantity['value'])) {
+                    return false;
+                }
+
+                $key = $quantity['position'];
+                $countString += $quantity['value'];
 
                 break;
 
             default:
                 return false;
         }
-
     }
 
     return $countString === count($results);
@@ -105,4 +141,4 @@ function getQuantityBySpecialCharacter(array $characters, int $position): ?array
 }
 
 // keep this function call here
-echo WildcardCharacters('*{5}+++ gheeeeejkl') ? 'true' : ' false';
+echo WildcardCharacters('++*{5} gheeeee') ? 'true' : ' false';
